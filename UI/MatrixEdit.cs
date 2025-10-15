99b84ccf9109f48b71b48860fee76531672ef76e
Breakcrumps <sonickita7@gmail.com>
2025-10-15 21:21:22 +0300

    Better

--- UI/MatrixEdit.cs
+++ UI/MatrixEdit.cs
@@ -1,23 +1,56 @@
 using Godot;
-using WorldTransformation.UI;
+using WorldTransformation.Algo;
+using WorldTransformation.Types;
 
 [GlobalClass]
 internal sealed partial class MatrixEdit : TextEdit
 {
-  [Export] private SubmitButton? _submitButton;
-  
+  [Export] private Transformer? _transformer;
+
   public override void _UnhandledInput(InputEvent @event)
   {
     if (@event is not InputEventKey eventKey)
       return;
 
     if (eventKey.IsActionPressed("MatrixEdit"))
-      GrabFocus();
+    {
+      if (!HasFocus())
+        GrabFocus();
+      else
+        ReleaseFocus();
+    }
 
     if (!HasFocus())
       return;
 
     if (eventKey.Keycode == Key.Enter && eventKey.ShiftPressed)
-      _submitButton?.SubmitMatrix();
+      SubmitMatrix();
+  }
+  
+  internal void SubmitMatrix()
+  {
+    if (_transformer is null)
+      return;
+    
+    string[] elems = Text.StripEdges().Replace('\n', ' ').Split(' ');
+
+    if (elems.Length != 9)
+      return;
+
+    float[] nums = new float[9];
+
+    for (int i = 0; i < 9; i++)
+    {
+      if (!float.TryParse(elems[i], out float num))
+        return;
+
+      nums[i] = num;
+    }
+
+    _transformer.Transform = Matrix.FromArray(nums);
+    _transformer.ExecuteTransform();
+
+    Clear();
+    ReleaseFocus();
   }
 }
--- UI/SubmitButton.cs
+++ UI/SubmitButton.cs
@@ -1,42 +1,15 @@
 using Godot;
-using WorldTransformation.Algo;
-using WorldTransformation.Types;
 
 namespace WorldTransformation.UI;
 
 [GlobalClass]
 internal sealed partial class SubmitButton : Button
 {
-  [Export] private TextEdit? _textEdit;
-  [Export] private Transformer? _transformer;
-  
+  [Export] private MatrixEdit? _matrixEdit;
+
   public override void _Ready()
-    => Pressed += SubmitMatrix;
-
-  internal void SubmitMatrix()
   {
-    if (_textEdit is null || _transformer is null)
-      return;
-
-    string[] elems = _textEdit.Text.StripEdges().Replace('\n', ' ').Split(' ');
-
-    if (elems.Length != 9)
-      return;
-
-    float[] nums = new float[9];
-
-    for (int i = 0; i < 9; i++)
-    {
-      if (!float.TryParse(elems[i], out float num))
-        return;
-
-      nums[i] = num;
-    }
-
-    _transformer.Transform = Matrix.FromArray(nums);
-    _transformer.ExecuteTransform();
-
-    _textEdit.Clear();
-    _textEdit.ReleaseFocus();
+    if (_matrixEdit is not null)
+      Pressed += _matrixEdit.SubmitMatrix;
   }
 }
